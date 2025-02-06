fb79626254aa5d1ded6c34e871e24aa5e44ee201
John Rhuel Laurente <22-1-00109@vsu.edu.ph>
2025-02-06 12:07:15 +0800

    Updates

---
+++ Food-Inventory-System/Inventory/InventoryUI.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Food_Inventory_System.Inventory
+{
+    partial class InventoryUI
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InventoryUI));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.foodTableView = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.def = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.foodTableView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.BorderRadius = 12;
+            this.guna2Button1.CheckedState.Parent = this.guna2Button1;
+            this.guna2Button1.CustomImages.Parent = this.guna2Button1;
+            this.guna2Button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.HoverState.Parent = this.guna2Button1;
+            this.guna2Button1.Image = ((System.Drawing.Image)(resources.GetObject("guna2Button1.Image")));
+            this.guna2Button1.Location = new System.Drawing.Point(1115, 9);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.ShadowDecoration.Parent = this.guna2Button1;
+            this.guna2Button1.Size = new System.Drawing.Size(187, 57);
+            this.guna2Button1.TabIndex = 0;
+            this.guna2Button1.Text = "Add Food";
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.BorderRadius = 12;
+            this.guna2Button2.CheckedState.Parent = this.guna2Button2;
+            this.guna2Button2.CustomImages.Parent = this.guna2Button2;
+            this.guna2Button2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(150)))), ((int)(((byte)(62)))));
+            this.guna2Button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button2.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.HoverState.Parent = this.guna2Button2;
+            this.guna2Button2.Image = ((System.Drawing.Image)(resources.GetObject("guna2Button2.Image")));
+            this.guna2Button2.Location = new System.Drawing.Point(956, 9);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.ShadowDecoration.Parent = this.guna2Button2;
+            this.guna2Button2.Size = new System.Drawing.Size(153, 57);
+            this.guna2Button2.TabIndex = 1;
+            this.guna2Button2.Text = "Export";
+            // 
+            // guna2TextBox1
+            // 
+            this.guna2TextBox1.BorderRadius = 20;
+            this.guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextBox1.DefaultText = "";
+            this.guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.DisabledState.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.FillColor = System.Drawing.Color.WhiteSmoke;
+            this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.FocusedState.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.ForeColor = System.Drawing.Color.Black;
+            this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.HoverState.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.IconLeft = ((System.Drawing.Image)(resources.GetObject("guna2TextBox1.IconLeft")));
+            this.guna2TextBox1.IconLeftOffset = new System.Drawing.Point(10, 0);
+            this.guna2TextBox1.Location = new System.Drawing.Point(20, 74);
+            this.guna2TextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.guna2TextBox1.Name = "guna2TextBox1";
+            this.guna2TextBox1.PasswordChar = '\0';
+            this.guna2TextBox1.PlaceholderForeColor = System.Drawing.Color.Black;
+            this.guna2TextBox1.PlaceholderText = "";
+            this.guna2TextBox1.SelectedText = "";
+            this.guna2TextBox1.ShadowDecoration.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.Size = new System.Drawing.Size(258, 44);
+            this.guna2TextBox1.TabIndex = 2;
+            // 
+            // foodTableView
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(239)))), ((int)(((byte)(212)))));
+            this.foodTableView.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.foodTableView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.foodTableView.BackgroundColor = System.Drawing.Color.White;
+            this.foodTableView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.foodTableView.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.foodTableView.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.foodTableView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.foodTableView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.foodTableView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.def});
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(244)))), ((int)(((byte)(226)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(221)))), ((int)(((byte)(160)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.foodTableView.DefaultCellStyle = dataGridViewCellStyle3;
+            this.foodTableView.EnableHeadersVisualStyles = false;
+            this.foodTableView.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(187)))), ((int)(((byte)(238)))), ((int)(((byte)(208)))));
+            this.foodTableView.Location = new System.Drawing.Point(20, 125);
+            this.foodTableView.Name = "foodTableView";
+            this.foodTableView.RowHeadersVisible = false;
+            this.foodTableView.RowHeadersWidth = 51;
+            this.foodTableView.RowTemplate.Height = 24;
+            this.foodTableView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.foodTableView.Size = new System.Drawing.Size(1266, 677);
+            this.foodTableView.TabIndex = 3;
+            this.foodTableView.Theme = Guna.UI2.WinForms.Enums.DataGridViewPresetThemes.Emerald;
+            this.foodTableView.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(239)))), ((int)(((byte)(212)))));
+            this.foodTableView.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.foodTableView.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.foodTableView.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.foodTableView.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.foodTableView.ThemeStyle.BackColor = System.Drawing.Color.White;
+            this.foodTableView.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(187)))), ((int)(((byte)(238)))), ((int)(((byte)(208)))));
+            this.foodTableView.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.foodTableView.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.foodTableView.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            this.foodTableView.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.foodTableView.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.foodTableView.ThemeStyle.HeaderStyle.Height = 27;
+            this.foodTableView.ThemeStyle.ReadOnly = false;
+            this.foodTableView.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(244)))), ((int)(((byte)(226)))));
+            this.foodTableView.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.foodTableView.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            this.foodTableView.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.Black;
+            this.foodTableView.ThemeStyle.RowsStyle.Height = 24;
+            this.foodTableView.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(221)))), ((int)(((byte)(160)))));
+            this.foodTableView.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.Black;
+            // 
+            // def
+            // 
+            this.def.HeaderText = "Column1";
+            this.def.MinimumWidth = 6;
+            this.def.Name = "def";
+            // 
+            // InventoryUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.foodTableView);
+            this.Controls.Add(this.guna2TextBox1);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.guna2Button1);
+            this.Name = "InventoryUI";
+            this.Size = new System.Drawing.Size(1324, 833);
+            this.Load += new System.EventHandler(this.InventoryUI_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.foodTableView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private Guna.UI2.WinForms.Guna2DataGridView foodTableView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn def;
+    }
+}
---
+++ Food-Inventory-System/Inventory/InventoryUI.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Food_Inventory_System.Classes;
+using Microsoft.VisualBasic.ApplicationServices;
+
+namespace Food_Inventory_System.Inventory
+{
+    public partial class InventoryUI : UserControl
+    {
+        public InventoryUI()
+        {
+            InitializeComponent();
+            RefreshTable(null);
+        }
+
+        private void InventoryUI_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void RefreshTable(List<Food> foods, int n = 60)
+        {
+            // Reset Textboxes
+          //  userTypeCB.Text = viewCountCB.Text = "";
+
+            // Reset Table
+            if (foodTableView.Columns.Contains("Action"))
+            {
+                foodTableView.Columns["Action"].Visible = false;
+                foodTableView.Columns.Remove("Action");
+            }
+            foodTableView.DataSource = null;
+            List<Food> u2 = null;
+            if (n == 60)
+                u2 = foods.GetRange(0, foods.Count);
+            else
+                u2 = foods.GetRange(0, n);
+
+            // Set up Table Style
+            foodTableView.DataSource = u2;
+            foodTableView.DefaultCellStyle.Font = new Font("Century Gothic", 12);
+            foodTableView.DefaultCellStyle.ForeColor = Color.Black;
+            foodTableView.AllowUserToResizeRows = false;
+            foodTableView.ColumnHeadersDefaultCellStyle.SelectionForeColor = Color.White;
+            foodTableView.ColumnHeadersDefaultCellStyle.SelectionBackColor = Color.FromArgb(40, 167, 69);
+            foodTableView.ColumnHeadersDefaultCellStyle.Font = new Font("Century Gothic", 14, FontStyle.Bold);
+            foodTableView.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            foodTableView.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            foodTableView.RowTemplate.Height = 40;
+            foodTableView.Columns["foodID"].Visible = false;
+
+            foodTableView.Columns[0].Width = 30;
+
+            // Add Action Column
+            DataGridViewComboBoxColumn comboBoxColumn = new DataGridViewComboBoxColumn();
+            comboBoxColumn.Items.AddRange("Update", "View", "Delete");
+            comboBoxColumn.Name = "Action";
+            comboBoxColumn.HeaderText = "Action";
+            comboBoxColumn.ValueType = typeof(String);
+            foodTableView.Columns.Add(comboBoxColumn);
+            foodTableView.EditingControlShowing += new DataGridViewEditingControlShowingEventHandler(foodTableView_EditingControlShowing);
+
+
+            // Set up Column Header
+            foodTableView.Columns["UserId"].HeaderText = "User ID";
+            foodTableView.Columns["foodName"].HeaderText = "Food Name";
+            foodTableView.Columns["quantity"].HeaderText = "Quantity";
+            foodTableView.Columns["category"].HeaderText = "Category";
+            foodTableView.Columns["expiryDate"].HeaderText = "Expiry Data";
+            foodTableView.Columns["storageLocation"].HeaderText = "Storage Location";
+            foodTableView.Columns["status"].HeaderText = "Status";
+        }
+
+        private void foodTableView_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            ComboBox combo = e.Control as ComboBox;
+            if (combo != null)
+            {
+                combo.SelectedIndexChanged -= new EventHandler(ComboBoxAction_SelectedIndexChanged);
+                combo.SelectedIndexChanged += new EventHandler(ComboBoxAction_SelectedIndexChanged);
+            }
+        }
+
+        private void ComboBoxAction_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox cb = (ComboBox)sender;
+            if (cb.SelectedItem != null)
+            {
+                int i = foodTableView.CurrentRow.Index;
+                if (cb.SelectedItem.ToString() == "Update")
+                {
+                   /* UpdateForm update = new UpdateForm();
+                    SetFormLocation(update);
+                    update.SetInformation(users, i);
+                    update.Owner = form;
+                    update.Show();*/
+                }
+                else if (cb.SelectedItem.ToString() == "View")
+                {
+                   /* ViewForm viewForm = new ViewForm();
+                    SetFormLocation(viewForm);
+                    viewForm.SetInformation(users, i);
+                    viewForm.Owner = form;
+                    viewForm.Show();*/
+                }
+                else if (cb.SelectedItem.ToString() == "Delete")
+                {
+                    /*DialogResult result = MessageBox.Show("Do you want to delete " + users[i].FirstName + " " + users[i].LastName + "" +
+                        "'s Information?\nThis action is irreversible.", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                    if (result == DialogResult.Yes)
+                    {
+                        MessageBox.Show(users[i].FirstName + " " + users[i].LastName + "'s information was sucesfully deleted.", "Sucessfull", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        db.DeleteUser(users[i].UserId);
+                        RefreshTable(users);
+                        refreshButtton_Click(sender, e);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Deletion was cancelled", "Deletion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }*/
+                }
+               
+            }
+        }
+    }
+}
