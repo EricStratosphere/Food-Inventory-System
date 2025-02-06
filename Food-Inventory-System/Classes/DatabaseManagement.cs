87effb95ed42266f3532e930f7add76bac6e1e5e
Stratosphere~ <stratospherestu@gmail.com>
2025-02-06 12:33:22 +0800

    uopdates

--- Food-Inventory-System/Classes/DatabaseManagement.cs
+++ Food-Inventory-System/Classes/DatabaseManagement.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 
 namespace Food_Inventory_System.Classes
@@ -26,7 +27,7 @@ namespace Food_Inventory_System.Classes
             Food = new List<Food>();     
         }
 
-        public List<Food> FetchAllFoods()
+       /* public List<Food> FetchAllFoods()
         {
             this.con = $"server={this.server};port=3306;uid={uid};pwd={pwd};database=food";
             using (MySqlConnection connection = new MySqlConnection(con))
@@ -40,19 +41,95 @@ namespace Food_Inventory_System.Classes
                         {
                             while (reader.Read())
                             {
-                                Food.Add(new Classes.Food
-                                {
-                                    FoodID = reader.GetString("FoodID"),
-                                    FoodName = reader.GetString("Name"),
-                                    Quantity = reader.
-
-                                });
+                                
                             }
                         }
                     }
                 }
             }
                 return this.Food;
+        }*/
+
+        public List<Food> GetAllFoodsByExpDesc()
+        {
+            Food.Clear();
+            using (MySqlConnection connection = new MySqlConnection(con))
+            {
+                connection.Open();
+                try
+                {
+                    using (MySqlCommand command = new MySqlCommand("SELECT * FROM application.food ORDER BY ExpiryDate DESC", connection))
+                    {
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Food.Add(new Food(
+                                (string)reader["FoodID"],
+                                (string)reader["FoodName"],
+                                (string)reader["Quantity"],
+                                (Category)Enum.Parse(typeof(Category), reader["Category"].ToString()),
+                                (DateTime)reader["ExpiryDate"],
+                                (StorageLocation)Enum.Parse(typeof(StorageLocation), reader["StorageLocation"].ToString()),
+                                (Status)Enum.Parse(typeof(Status), reader["Status"].ToString())
+                            ));
+                            }
+                        }
+                    }
+                    return Food;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("There was an error while retrieving users: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+            return Food;
+
+        }
+
+        public List<Food> GetAllFoodsByExpAsc()
+        {
+            Food.Clear();
+            using (MySqlConnection connection = new MySqlConnection(con))
+            {
+                connection.Open();
+                try
+                {
+                    using (MySqlCommand command = new MySqlCommand("SELECT * FROM application.food ORDER BY ExpiryDate ASC", connection))
+                    {
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Food.Add(new Food(
+                                (string)reader["FoodID"],
+                                (string)reader["FoodName"],
+                                (string)reader["Quantity"],
+                                (Category)Enum.Parse(typeof(Category), reader["Category"].ToString()),
+                                (DateTime)reader["ExpiryDate"],
+                                (StorageLocation)Enum.Parse(typeof(StorageLocation), reader["StorageLocation"].ToString()),
+                                (Status)Enum.Parse(typeof(Status), reader["Status"].ToString())
+                            ));
+                            }
+                        }
+                    }
+                    return Food;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("There was an error while retrieving users: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+            return Food;
+
         }
     }
 }
